9ca1f84bb22d472fab029b62872139fd3ee2953b
Ed Scott <e.scott15@ic.ac.uk>
2018-11-27 15:39:27 +0000

    add ability to define classes using multi-class BDT in significance calculation

--- TwoStep/dataSignificances.py
+++ TwoStep/dataSignificances.py
@@ -8,10 +8,11 @@ import pickle
 from sklearn.metrics import roc_auc_score, roc_curve
 from os import path, system
 
-from addRowFunctions import addPt, truthDipho, reco, diphoWeight, altDiphoWeight
-from otherHelpers import prettyHist, getAMS, computeBkg, getRealSigma
+from addRowFunctions import addPt, truthDipho, reco, diphoWeight, altDiphoWeight, truthClass
+from otherHelpers import prettyHist, getAMS, computeBkg, getRealSigma, jetPtToClass
 from root_numpy import tree2array, fill_hist
 import usefulStyle as useSty
+from catOptim import CatOptim
 
 #configure options
 from optparse import OptionParser
@@ -20,6 +21,7 @@ parser.add_option('-t','--trainDir', help='Directory for input files')
 parser.add_option('-d','--dataFrame', default=None, help='Name of dataframe if it already exists')
 parser.add_option('-s','--signifFrame', default=None, help='Name of cleaned signal dataframe if it already exists')
 parser.add_option('-m','--modelName', default=None, help='Name of model for testing')
+parser.add_option('-c','--className', default=None, help='Name of multi-class model used to build categories. If None, use reco categories')
 parser.add_option('-n','--nIterations', default=1000, help='Number of iterations to run for random significance optimisation')
 parser.add_option('--intLumi',type='float', default=35.9, help='Integrated luminosity')
 (opts,args)=parser.parse_args()
@@ -30,7 +32,9 @@ if trainDir.endswith('/'): trainDir = trainDir[:-1]
 frameDir = trainDir.replace('trees','frames')
 modelDir = trainDir.replace('trees','models')
 plotDir  = trainDir.replace('trees','plots')
+nJetClasses = 3
 nClasses = 9
+jetPriors = [0.606560, 0.270464, 0.122976]
 
 #put root in batch mode
 r.gROOT.SetBatch(True)
@@ -44,6 +48,13 @@ diphoVars  = ['leadmva','subleadmva','leadptom','subleadptom',
               'leadeta','subleadeta',
               'CosPhi','vtxprob','sigmarv','sigmawv']
 
+jetVars  = ['n_rec_jets','dijet_Mjj',
+              'dijet_leadEta','dijet_subleadEta','dijet_subsubleadEta',
+              'dijet_LeadJPt','dijet_SubJPt','dijet_SubsubJPt',
+              'dijet_leadPUMVA','dijet_subleadPUMVA','dijet_subsubleadPUMVA',
+              'dijet_leadDeltaPhi','dijet_subleadDeltaPhi','dijet_subsubleadDeltaPhi',
+              'dijet_leadDeltaEta','dijet_subleadDeltaEta','dijet_subsubleadDeltaEta']
+
 #either get existing data frame or create it
 trainTotal = None
 if not opts.dataFrame:
@@ -55,11 +66,9 @@ if not opts.dataFrame:
       else: trainTree = trainFile.Get('vbfTagDumper/trees/%s_13TeV_VBFDiJet'%proc)
       trainTree.SetBranchStatus('nvtx',0)
       trainTree.SetBranchStatus('VBFMVAValue',0)
-      trainTree.SetBranchStatus('dijet_*',0)
       trainTree.SetBranchStatus('dZ',0)
       trainTree.SetBranchStatus('centralObjectWeight',0)
       trainTree.SetBranchStatus('rho',0)
-      trainTree.SetBranchStatus('nvtx',0)
       trainTree.SetBranchStatus('event',0)
       trainTree.SetBranchStatus('lumi',0)
       trainTree.SetBranchStatus('processIndex',0)
@@ -114,7 +123,8 @@ else:
 
 if not opts.signifFrame:
   #read in signal mc dataframe
-  trainTotal = pd.read_pickle('%s/trainTotal.pkl'%frameDir)
+  #trainTotal = pd.read_pickle('%s/trainTotal.pkl'%frameDir)
+  trainTotal = pd.read_pickle('%s/jetTotal.pkl'%frameDir)
   print 'Successfully loaded the signal dataframe'
   
   #remove bkg then add reco tag info
@@ -125,6 +135,8 @@ if not opts.signifFrame:
   print 'About to add reco tag info'
   trainTotal['diphopt'] = trainTotal.apply(addPt, axis=1)
   trainTotal['reco'] = trainTotal.apply(reco, axis=1)
+  trainTotal['truthDipho'] = trainTotal.apply(truthDipho, axis=1)
+  trainTotal['truthClass'] = trainTotal.apply(truthClass, axis=1)
   print 'Successfully added reco tag info'
 
   #save
@@ -140,14 +152,19 @@ else:
 #define the variables used as input to the classifier
 diphoX  = trainTotal[diphoVars].values
 diphoY  = trainTotal['truthDipho'].values
+diphoI  = trainTotal[jetVars].values
+diphoJ  = trainTotal['truthClass'].values
 diphoFW = trainTotal['weight'].values
 diphoS  = trainTotal['stage1cat'].values
+diphoP  = trainTotal['diphopt'].values
 diphoR  = trainTotal['reco'].values
 diphoM  = trainTotal['CMS_hgg_mass'].values
 
 dataX  = dataTotal[diphoVars].values
+dataI  = dataTotal[jetVars].values
 dataY  = np.zeros(dataX.shape[0])
 dataFW = np.ones(dataX.shape[0])
+dataP  = dataTotal['diphopt'].values
 dataR  = dataTotal['reco'].values
 dataM  = dataTotal['CMS_hgg_mass'].values
 
@@ -155,7 +172,7 @@ dataM  = dataTotal['CMS_hgg_mass'].values
 diphoMatrix = xg.DMatrix(diphoX, label=diphoY, weight=diphoFW, feature_names=diphoVars)
 dataMatrix  = xg.DMatrix(dataX,  label=dataY,  weight=dataFW,  feature_names=diphoVars)
 
-#loadi the model to be tested
+#load the dipho model to be tested
 diphoModel = xg.Booster()
 diphoModel.load_model('%s/%s'%(modelDir,opts.modelName))
 
@@ -163,137 +180,39 @@ diphoModel.load_model('%s/%s'%(modelDir,opts.modelName))
 diphoPredY = diphoModel.predict(diphoMatrix)
 dataPredY  = diphoModel.predict(dataMatrix)
 
+#load the class model to be tested, if it exists
+if opts.className:
+  classModel = xg.Booster()
+  classModel.load_model('%s/%s'%(modelDir,opts.className))
+
+  if 'jet' in opts.className:
+    classMatrix = xg.DMatrix(diphoI, label=diphoY, weight=diphoFW, feature_names=jetVars)
+    predProbJet = classModel.predict(classMatrix).reshape(diphoX.shape[0],nJetClasses)
+    predProbJet *= jetPriors
+    classPredJ = np.argmax(predProbJet, axis=1)
+    diphoR = jetPtToClass(classPredJ, diphoP)
+
+    classDataMatrix = xg.DMatrix(dataI, label=dataY, weight=dataFW, feature_names=jetVars)
+    predProbJet = classModel.predict(classDataMatrix).reshape(dataX.shape[0],nJetClasses)
+    predProbJet *= jetPriors
+    classPredJ = np.argmax(predProbJet, axis=1)
+    dataR = jetPtToClass(classPredJ, dataP)
+  else:
+    raise Exception("your class model type is not yet supported, sorry")
+
 #now estimate two-class significance
+#set up parameters for the optimiser
+ranges = [ [0.5,1.] ]
+names  = ['DiphotonBDT']
 printStr = ''
-lumi = opts.intLumi
-for iProc in range(nClasses):
-  bestCutLo = -.1
-  bestCutHi = -.1
-  bestSignifLo = -1.
-  bestSignifHi = -1.
-  bestSlo = -1.
-  bestShi = -1.
-  bestBlo = -1.
-  bestBhi = -1.
-  bestSignif = -1.
-  for i in range(opts.nIterations): 
-    cuts = np.random.uniform(0.,1.,2)
-    cuts.sort()
-    cutLo = cuts[0]
-    cutHi = cuts[1]
-    sigHistHi = r.TH1F('sigHistHiTemp','sigHistHiTemp',160,100,180)
-    sigWeightsHi = diphoFW * (diphoY==1) * (diphoS-3==iProc) * (diphoPredY>cutHi) * (diphoR==iProc)
-    fill_hist(sigHistHi, diphoM, weights=sigWeightsHi)
-    sigCountHi = 0.68 * lumi * sigHistHi.Integral() 
-    sigWidthHi = getRealSigma(sigHistHi)
-    bkgHistHi = r.TH1F('bkgHistHiTemp','bkgHistHiTemp',160,100,180)
-    bkgWeightsHi = dataFW * (dataPredY>cutHi) * (dataR==iProc)
-    fill_hist(bkgHistHi, dataM, weights=bkgWeightsHi)
-    bkgCountHi = computeBkg(bkgHistHi, sigWidthHi)
-    theSignifHi = getAMS(sigCountHi, bkgCountHi)
-    sigHistLo = r.TH1F('sigHistLoTemp','sigHistLoTemp',160,100,180)
-    sigWeightsLo = diphoFW * (diphoY==1) * (diphoS-3==iProc) * (diphoPredY<cutHi) * (diphoPredY>cutLo) * (diphoR==iProc)
-    fill_hist(sigHistLo, diphoM, weights=sigWeightsLo)
-    sigCountLo = 0.68 * lumi * sigHistLo.Integral() 
-    sigWidthLo = getRealSigma(sigHistLo)
-    #print 'sigwidth is %1.3f'%sigWidth
-    bkgHistLo = r.TH1F('bkgHistLoTemp','bkgHistLoTemp',160,100,180)
-    bkgWeightsLo = dataFW * (dataPredY<cutHi) * (dataPredY>cutLo) * (dataR==iProc)
-    fill_hist(bkgHistLo, dataM, weights=bkgWeightsLo)
-    bkgCountLo = computeBkg(bkgHistLo, sigWidthLo)
-    theSignifLo = getAMS(sigCountLo, bkgCountLo)
-    theSignif = np.sqrt( theSignifLo*theSignifLo + theSignifHi*theSignifHi )
-    if theSignif > bestSignif: 
-      bestCutLo = cutLo
-      bestCutHi = cutHi
-      bestSignif = theSignif
-      bestSignifLo = theSignifLo
-      bestSignifHi = theSignifHi
-      bestSlo = sigCountLo
-      bestBlo = bkgCountLo
-      bestShi = sigCountHi
-      bestBhi = bkgCountHi
-  printStr += 'for proc %g the best outcome was tot signif %1.2f:\n'%(iProc,bestSignif)
-  printStr += 'cutHi %1.3f, Shi %1.2f, Bhi %1.2f, signifHi %1.2f \n'%(bestCutHi,bestShi,bestBhi,bestSignifHi)
-  printStr += 'cutLo %1.3f, Slo %1.2f, Blo %1.2f, signifLo %1.2f \n'%(bestCutLo,bestSlo,bestBlo,bestSignifLo)
-  printStr += '\n'
 
-  #now do some checking that random search found a good minimum
-  cutLoCheck = r.TGraph()
-  cutLoCheck.SetName('cutLoCheck_%g'%iProc)
-  useSty.formatHisto(cutLoCheck.GetHistogram())
-  cutLoCheck.GetXaxis().SetTitle('Change in cut')
-  cutLoCheck.GetYaxis().SetTitle('Significance')
-  cutLoCheck.SetTitle('')
-  for iCutLo,cutLo in enumerate(np.arange(bestCutLo-0.1, bestCutLo+0.1, 0.01)):
-    cutHi = bestCutHi
-    sigHistHi = r.TH1F('sigHistHiTemp','sigHistHiTemp',160,100,180)
-    sigWeightsHi = diphoFW * (diphoY==1) * (diphoS-3==iProc) * (diphoPredY>cutHi) * (diphoR==iProc)
-    fill_hist(sigHistHi, diphoM, weights=sigWeightsHi)
-    sigCountHi = 0.68 * lumi * sigHistHi.Integral() 
-    sigWidthHi = getRealSigma(sigHistHi)
-    bkgHistHi = r.TH1F('bkgHistHiTemp','bkgHistHiTemp',160,100,180)
-    bkgWeightsHi = dataFW * (dataPredY>cutHi) * (dataR==iProc)
-    fill_hist(bkgHistHi, dataM, weights=bkgWeightsHi)
-    bkgCountHi = computeBkg(bkgHistHi, sigWidthHi)
-    theSignifHi = getAMS(sigCountHi, bkgCountHi)
-    sigHistLo = r.TH1F('sigHistLoTemp','sigHistLoTemp',160,100,180)
-    sigWeightsLo = diphoFW * (diphoY==1) * (diphoS-3==iProc) * (diphoPredY<cutHi) * (diphoPredY>cutLo) * (diphoR==iProc)
-    fill_hist(sigHistLo, diphoM, weights=sigWeightsLo)
-    sigCountLo = 0.68 * lumi * sigHistLo.Integral() 
-    sigWidthLo = getRealSigma(sigHistLo)
-    #print 'sigwidth is %1.3f'%sigWidth
-    bkgHistLo = r.TH1F('bkgHistLoTemp','bkgHistLoTemp',160,100,180)
-    bkgWeightsLo = dataFW * (dataPredY<cutHi) * (dataPredY>cutLo) * (dataR==iProc)
-    fill_hist(bkgHistLo, dataM, weights=bkgWeightsLo)
-    bkgCountLo = computeBkg(bkgHistLo, sigWidthLo)
-    theSignifLo = getAMS(sigCountLo, bkgCountLo)
-    theSignif = np.sqrt( theSignifLo*theSignifLo + theSignifHi*theSignifHi )
-    cutLoCheck.SetPoint(iCutLo, cutLo - bestCutLo, theSignif)
-  theCanv = useSty.setCanvas()
-  cutLoCheck.Draw()
-  useSty.drawCMS()
-  useSty.drawEnPu(lumi='%2.1f fb^{-1}'%opts.intLumi)
-  theCanv.Print('%s/%s__%s.pdf'%(plotDir, opts.modelName, cutLoCheck.GetName()))
-  theCanv.Print('%s/%s__%s.png'%(plotDir, opts.modelName, cutLoCheck.GetName()))
+for iClass in range(nClasses):
+  sigWeights = diphoFW * (diphoJ==iClass) * (diphoR==iClass)
+  bkgWeights = dataFW * (dataR==iClass)
+  optimiser = CatOptim(sigWeights, diphoM, [diphoPredY], bkgWeights, dataM, [dataPredY], 2, ranges, names)
+  optimiser.optimise(opts.intLumi, opts.nIterations)
+  printStr += 'Results for bin %g : \n'%iClass
+  printStr += optimiser.getPrintableResult()
 
-  #and for the high cut
-  cutHiCheck = r.TGraph()
-  cutHiCheck.SetName('cutHiCheck_%g'%iProc)
-  useSty.formatHisto(cutHiCheck.GetHistogram())
-  cutHiCheck.GetXaxis().SetTitle('Change in cut')
-  cutHiCheck.GetYaxis().SetTitle('Significance')
-  cutHiCheck.SetTitle('')
-  for iCutHi,cutHi in enumerate(np.arange(bestCutHi-0.1, bestCutHi+0.1, 0.01)):
-    cutLo = bestCutLo
-    sigHistHi = r.TH1F('sigHistHiTemp','sigHistHiTemp',160,100,180)
-    sigWeightsHi = diphoFW * (diphoY==1) * (diphoS-3==iProc) * (diphoPredY>cutHi) * (diphoR==iProc)
-    fill_hist(sigHistHi, diphoM, weights=sigWeightsHi)
-    sigCountHi = 0.68 * lumi * sigHistHi.Integral() 
-    sigWidthHi = getRealSigma(sigHistHi)
-    bkgHistHi = r.TH1F('bkgHistHiTemp','bkgHistHiTemp',160,100,180)
-    bkgWeightsHi = dataFW * (dataPredY>cutHi) * (dataR==iProc)
-    fill_hist(bkgHistHi, dataM, weights=bkgWeightsHi)
-    bkgCountHi = computeBkg(bkgHistHi, sigWidthHi)
-    theSignifHi = getAMS(sigCountHi, bkgCountHi)
-    sigHistLo = r.TH1F('sigHistLoTemp','sigHistLoTemp',160,100,180)
-    sigWeightsLo = diphoFW * (diphoY==1) * (diphoS-3==iProc) * (diphoPredY<cutHi) * (diphoPredY>cutLo) * (diphoR==iProc)
-    fill_hist(sigHistLo, diphoM, weights=sigWeightsLo)
-    sigCountLo = 0.68 * lumi * sigHistLo.Integral() 
-    sigWidthLo = getRealSigma(sigHistLo)
-    #print 'sigwidth is %1.3f'%sigWidth
-    bkgHistLo = r.TH1F('bkgHistLoTemp','bkgHistLoTemp',160,100,180)
-    bkgWeightsLo = dataFW * (dataPredY<cutHi) * (dataPredY>cutLo) * (dataR==iProc)
-    fill_hist(bkgHistLo, dataM, weights=bkgWeightsLo)
-    bkgCountLo = computeBkg(bkgHistLo, sigWidthLo)
-    theSignifLo = getAMS(sigCountLo, bkgCountLo)
-    theSignif = np.sqrt( theSignifLo*theSignifLo + theSignifHi*theSignifHi )
-    cutHiCheck.SetPoint(iCutHi, cutHi - bestCutHi, theSignif)
-  theCanv = useSty.setCanvas()
-  cutHiCheck.Draw()
-  useSty.drawCMS()
-  useSty.drawEnPu(lumi='%2.1f fb^{-1}'%opts.intLumi)
-  theCanv.Print('%s/%s__%s.pdf'%(plotDir, opts.modelName, cutHiCheck.GetName()))
-  theCanv.Print('%s/%s__%s.png'%(plotDir, opts.modelName, cutHiCheck.GetName()))
-
+print
 print printStr
