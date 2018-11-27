0243ccffcfb368f645e7ab286a91ea9ccac13bbc
Ed Scott <e.scott15@ic.ac.uk>
2018-11-27 15:39:27 +0000

    add ability to define classes using multi-class BDT in significance calculation

--- TwoStep/runJobs.py
+++ TwoStep/runJobs.py
@@ -17,45 +17,51 @@ intLumi = 35.9
 #script    = 'diphotonCategorisation.py'
 #paramSets = [None,'max_depth:3','max_depth:4','max_depth:5','max_depth:10','eta:0.1','eta:0.5','lambda:0']
 #models    = None
+#classModel = None
 ##dataFrame = 'trainTotal.pkl'
 #dataFrame = None
 #sigFrame  = None
 
-script    = 'nJetCategorisation.py'
-paramSets = [None,'max_depth:10']
-models    = None
-#dataFrame = 'jetTotal.pkl'
-dataFrame = None
-sigFrame  = None
-
-#script    = 'dataSignificances.py'
-#models    = ['altDiphoModel.model','diphoModel.model']
-#paramSets = [None,'max_depth:3','max_depth:4','max_depth:5','max_depth:10','eta:0.1','eta:0.5','lambda:0']
-#for params in paramSets:
-#  if not params: continue
-#  params = params.split(',')
-#  name = 'diphoModel'
-#  for param in params:
-#    var = param.split(':')[0]
-#    val = param.split(':')[1]
-#    name += '__%s_%s'%(var,str(val))
-#  name += '.model'
-#  models.append(name)
-#  models.append(name.replace('dipho','altDipho'))
-#paramSets = None
-##dataFrame = 'dataTotal.pkl'
+#script    = 'nJetCategorisation.py'
+#paramSets = [None,'max_depth:10']
+#models    = None
+#classModel = None
+##dataFrame = 'jetTotal.pkl'
 #dataFrame = None
-##sigFrame  = 'signifTotal.pkl'
 #sigFrame  = None
+
+script    = 'dataSignificances.py'
+models    = ['altDiphoModel.model','diphoModel.model']
+#paramSets = [None,'max_depth:3','max_depth:4','max_depth:5','max_depth:10','eta:0.1','eta:0.5','lambda:0']
+paramSets = [None]
+classModel = 'jetModel.model'
+for params in paramSets:
+  if not params: continue
+  params = params.split(',')
+  name = 'diphoModel'
+  for param in params:
+    var = param.split(':')[0]
+    val = param.split(':')[1]
+    name += '__%s_%s'%(var,str(val))
+  name += '.model'
+  models.append(name)
+  models.append(name.replace('dipho','altDipho'))
+paramSets = None
+#dataFrame = 'dataTotal.pkl'
+dataFrame = None
+#sigFrame  = 'signifTotal.pkl'
+sigFrame  = None
 
 #script    = 'dataMCcheckSidebands.py'
 #models    = ['altDiphoModel.model','diphoModel.model']
+#classModel = None
 #paramSets = None
 #dataFrame = 'dataTotal.pkl'
 #sigFrame  = 'trainTotal.pkl'
 
 #script    = 'dataSignificancesVBF.py'
 #models    = [None,'altDiphoModel.model','diphoModel.model']
+#classModel = None
 #paramSets = [None,'max_depth:3','max_depth:4','max_depth:5','max_depth:10','eta:0.1','eta:0.5','lambda:0']
 #for params in paramSets:
 #  if not params: continue
@@ -77,12 +83,14 @@ sigFrame  = None
 #script    = 'combinedBDT.py'
 #paramSets = None
 #models    = [None,'altDiphoModel.model']
+#classModel = None
 ##dataFrame = None
 #dataFrame = 'combinedTotal.pkl'
 #sigFrame  = None
 
 #script    = 'dataSignificancesVBFcombined.py'
 #models = [None,'altDiphoModel.model']
+#classModel = None
 #paramSets = None
 ##dataFrame = None
 #dataFrame = 'dataTotal.pkl'
@@ -102,6 +110,8 @@ if __name__=='__main__':
       theCmd += '-s %s '%sigFrame
     if intLumi: 
       theCmd += '--intLumi %s '%intLumi
+    if classModel: 
+      theCmd += '--className %s '%classModel
     if paramSets and models:
       exit('ERROR do not expect both parameter set options and models. Exiting..')
     elif paramSets: 
