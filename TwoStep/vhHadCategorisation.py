4161ee27846fc59f4d131adeb7dbec8fa147977e
Ed Scott <e.scott15@ic.ac.uk>
2019-06-07 13:53:56 +0100

    finish VH had BDT script

--- TwoStep/vhHadCategorisation.py
+++ TwoStep/vhHadCategorisation.py
@@ -1,20 +1,12 @@
 #usual imports
-import ROOT as r
 import numpy as np
 import pandas as pd
 import xgboost as xg
 import uproot as upr
-import matplotlib
-matplotlib.use('Agg')
-import matplotlib.pyplot as plt
 import pickle
 from sklearn.metrics import roc_auc_score, roc_curve
 from os import path, system
-
-from addRowFunctions import addPt, truthDipho, reco, diphoWeight, altDiphoWeight
-from otherHelpers import prettyHist, getAMS, computeBkg, getRealSigma
-from root_numpy import fill_hist
-import usefulStyle as useSty
+from addRowFunctions import truthVhHad, vhHadWeight
 
 #configure options
 from optparse import OptionParser
@@ -30,34 +22,30 @@ trainDir = opts.trainDir
 if trainDir.endswith('/'): trainDir = trainDir[:-1]
 frameDir = trainDir.replace('trees','frames')
 if opts.trainParams: opts.trainParams = opts.trainParams.split(',')
-trainFrac = 0.7
-validFrac = 0.1
 
-#get trees from files, put them in data frames
+#define input files
 procFileMap = {'ggh':'ggH.root', 'vbf':'VBF.root', 'vh':'VH.root'}
 theProcs = procFileMap.keys()
 
-#define the different sets of variables used; will want to revise this for VH
-allVars    = ['leadmva','subleadmva','leadptom','subleadptom',
-              'leadeta','subleadeta',
-              'CosPhi','vtxprob','sigmarv','sigmawv',
-              'weight', 'CMS_hgg_mass', 'HTXSstage0cat', 'HTXSstage1_1_cat', 'cosThetaStar']
-vhHadVars  = ['leadmva','subleadmva','leadptom','subleadptom',
-              'leadeta','subleadeta',
-              'CosPhi','vtxprob','sigmarv','sigmawv']
+#define variables to be used
+allVars    = ['dipho_leadIDMVA', 'dipho_subleadIDMVA', 'dipho_lead_ptoM', 'dipho_sublead_ptoM', 'dipho_mva', 
+              'dijet_leadEta', 'dijet_subleadEta', 'dijet_LeadJPt', 'dijet_SubJPt', 'dijet_abs_dEta', 'dijet_Mjj', 'dipho_dijet_ptHjj', 'dijet_dipho_dphi_trunc',
+              'cosThetaStar', 'dipho_cosphi', 'vtxprob', 'sigmarv', 'sigmawv', 'weight', 'HTXSstage0cat', 'HTXSstage1_1_cat', 'dipho_mass']
+
+vhHadVars  = ['dipho_lead_ptoM', 'dipho_sublead_ptoM', 'dijet_leadEta', 'dijet_subleadEta', 'dijet_LeadJPt', 'dijet_SubJPt', 'dijet_Mjj', 'dijet_abs_dEta', 'cosThetaStar']
 
 #either get existing data frame or create it
 trainTotal = None
 if not opts.dataFrame:
   trainFrames = {}
-  #get the trees, turn them into arrays
+  #get trees from files, put them in data frames
   for proc,fn in procFileMap.iteritems():
       trainFile   = upr.open('%s/%s'%(trainDir,fn))
       trainTree = trainFile['vbfTagDumper/trees/%s_13TeV_VBFDiJet'%proc]
       trainFrames[proc] = trainTree.pandas.df(allVars)
       trainFrames[proc]['proc'] = proc
   print 'got trees'
-  
+
   #create one total frame
   trainList = []
   for proc in theProcs:
@@ -65,119 +53,89 @@ if not opts.dataFrame:
   trainTotal = pd.concat(trainList)
   del trainFrames
   print 'created total frame'
-  
+
   #then filter out the events into only those with the phase space we are interested in
-  trainTotal = trainTotal[trainTotal.CMS_hgg_mass>100.]
-  trainTotal = trainTotal[trainTotal.CMS_hgg_mass<180.]
+  trainTotal = trainTotal[trainTotal.dipho_mass>100.]
+  trainTotal = trainTotal[trainTotal.dipho_mass<180.]
   print 'done mass cuts'
-  
-  #some extra cuts that are applied for vhHad BDT in the AN
-  trainTotal = trainTotal[trainTotal.leadmva>-0.9]
-  trainTotal = trainTotal[trainTotal.subleadmva>-0.9]
-  trainTotal = trainTotal[trainTotal.leadptom>0.333]
-  trainTotal = trainTotal[trainTotal.subleadptom>0.25]
-  trainTotal = trainTotal[trainTotal.stage1cat>-1.]
+  trainTotal = trainTotal[trainTotal.dipho_leadIDMVA>-0.9]
+  trainTotal = trainTotal[trainTotal.dipho_subleadIDMVA>-0.9]
+  trainTotal = trainTotal[trainTotal.dipho_lead_ptoM>0.333]
+  trainTotal = trainTotal[trainTotal.dipho_sublead_ptoM>0.25]
   print 'done basic preselection cuts'
-  
-  #add extra info to dataframe
-  print 'about to add extra columns'
-  trainTotal['truthDipho'] = trainTotal.apply(truthDipho,axis=1)
-  trainTotal['vhHadWeight'] = trainTotal.apply(vhHadWeight,axis=1)
-  trainTotal['altDiphoWeight'] = trainTotal.apply(altDiphoWeight, axis=1)
-  print 'all columns added'
+  trainTotal = trainTotal[trainTotal.dijet_LeadJPt>30.]
+  trainTotal = trainTotal[trainTotal.dijet_SubJPt>30.]
+  trainTotal = trainTotal[trainTotal.dijet_Mjj>60.]
+  trainTotal = trainTotal[trainTotal.dijet_Mjj<120.]
+  print 'done jet cuts'
+
+  #add the target variable and the equalised weight
+  trainTotal['truthVhHad'] = trainTotal.apply(truthVhHad,axis=1)
+  sigSumW = np.sum(trainTotal[trainTotal.truthVhHad>0.5]['weight'].values)
+  bkgSumW = np.sum(trainTotal[trainTotal.truthVhHad<0.5]['weight'].values)
+  print 'sigSumW, bkgSumW, ratio = %.3f, %.3f, %.3f'%(sigSumW, bkgSumW, sigSumW/bkgSumW)
+  trainTotal['vhHadWeight'] = trainTotal.apply(vhHadWeight, axis=1, args=[bkgSumW/sigSumW])
 
   #save as a pickle file
   if not path.isdir(frameDir): 
     system('mkdir -p %s'%frameDir)
-  trainTotal.to_pickle('%s/trainTotal.pkl'%frameDir)
-  print 'frame saved as %s/trainTotal.pkl'%frameDir
+  trainTotal.to_pickle('%s/vhHadTotal.pkl'%frameDir)
+  print 'frame saved as %s/vhHadTotal.pkl'%frameDir
 
 #read in dataframe if above steps done before
 else:
   trainTotal = pd.read_pickle('%s/%s'%(frameDir,opts.dataFrame))
   print 'Successfully loaded the dataframe'
 
-sigSumW = np.sum( trainTotal[trainTotal.stage1cat>0.01]['weight'].values )
-bkgSumW = np.sum( trainTotal[trainTotal.stage1cat==0]['weight'].values )
-print 'sigSumW %.6f'%sigSumW
-print 'bkgSumW %.6f'%bkgSumW
-print 'ratio %.6f'%(sigSumW/bkgSumW)
-#exit('first just count the weights')
-
-#define the indices shuffle (useful to keep this separate so it can be re-used)
+#set up train set and randomise the inputs
+trainFrac = 0.8
 theShape = trainTotal.shape[0]
-vhHadShuffle = np.random.permutation(theShape)
-vhHadTrainLimit = int(theShape*trainFrac)
-vhHadValidLimit = int(theShape*(trainFrac+validFrac))
+theShuffle = np.random.permutation(theShape)
+trainLimit = int(theShape*trainFrac)
 
-#setup the various datasets for vhHad training
+#define the values needed for training as numpy arrays
 vhHadX  = trainTotal[vhHadVars].values
-vhHadY  = trainTotal['truthDipho'].values
+vhHadY  = trainTotal['truthVhHad'].values
 vhHadTW = trainTotal['vhHadWeight'].values
-vhHadAW = trainTotal['altDiphoWeight'].values
 vhHadFW = trainTotal['weight'].values
-vhHadM  = trainTotal['CMS_hgg_mass'].values
-del trainTotal
+vhHadM  = trainTotal['dipho_mass'].values
 
-vhHadX  = vhHadX[vhHadShuffle]
-vhHadY  = vhHadY[vhHadShuffle]
-vhHadTW = vhHadTW[vhHadShuffle]
-vhHadAW = vhHadAW[vhHadShuffle]
-vhHadFW = vhHadFW[vhHadShuffle]
-vhHadM  = vhHadM[vhHadShuffle]
+#do the shuffle
+vhHadX  = vhHadX[theShuffle]
+vhHadY  = vhHadY[theShuffle]
+vhHadTW = vhHadTW[theShuffle]
+vhHadFW = vhHadFW[theShuffle]
+vhHadM  = vhHadM[theShuffle]
 
-vhHadTrainX,  vhHadValidX,  vhHadTestX  = np.split( vhHadX,  [vhHadTrainLimit,vhHadValidLimit] )
-vhHadTrainY,  vhHadValidY,  vhHadTestY  = np.split( vhHadY,  [vhHadTrainLimit,vhHadValidLimit] )
-vhHadTrainTW, vhHadValidTW, vhHadTestTW = np.split( vhHadTW, [vhHadTrainLimit,vhHadValidLimit] )
-vhHadTrainAW, vhHadValidAW, vhHadTestAW = np.split( vhHadAW, [vhHadTrainLimit,vhHadValidLimit] )
-vhHadTrainFW, vhHadValidFW, vhHadTestFW = np.split( vhHadFW, [vhHadTrainLimit,vhHadValidLimit] )
-vhHadTrainM,  vhHadValidM,  vhHadTestM  = np.split( vhHadM,  [vhHadTrainLimit,vhHadValidLimit] )
+#split into train and test
+vhHadTrainX,  vhHadTestX  = np.split( vhHadX,  [trainLimit] )
+vhHadTrainY,  vhHadTestY  = np.split( vhHadY,  [trainLimit] )
+vhHadTrainTW, vhHadTestTW = np.split( vhHadTW, [trainLimit] )
+vhHadTrainFW, vhHadTestFW = np.split( vhHadFW, [trainLimit] )
+vhHadTrainM,  vhHadTestM  = np.split( vhHadM,  [trainLimit] )
 
-#build the background discrimination BDT
-trainingDipho = xg.DMatrix(vhHadTrainX, label=vhHadTrainY, weight=vhHadTrainTW, feature_names=vhHadVars)
-testingDipho  = xg.DMatrix(vhHadTestX,  label=vhHadTestY,  weight=vhHadTestFW,  feature_names=vhHadVars)
+#set up the training and testing matrices
+trainMatrix = xg.DMatrix(vhHadTrainX, label=vhHadTrainY, weight=vhHadTrainTW, feature_names=vhHadVars)
+testMatrix  = xg.DMatrix(vhHadTestX, label=vhHadTestY, weight=vhHadTestFW, feature_names=vhHadVars)
 trainParams = {}
 trainParams['objective'] = 'binary:logistic'
 trainParams['nthread'] = 1
-paramExt = ''
-if opts.trainParams:
-  paramExt = '__'
-  for pair in opts.trainParams:
-    key  = pair.split(':')[0]
-    data = pair.split(':')[1]
-    trainParams[key] = data
-    paramExt += '%s_%s__'%(key,data)
-  paramExt = paramExt[:-2]
-print 'about to train vhHad BDT'
-vhHadModel = xg.train(trainParams, trainingDipho)
+
+#train the BDT
+print 'about to train diphoton BDT'
+vhHadModel = xg.train(trainParams, trainMatrix)
 print 'done'
 
-#save it
-modelDir = trainDir.replace('trees','models')
-if not path.isdir(modelDir):
-  system('mkdir -p %s'%modelDir)
-vhHadModel.save_model('%s/vhHadModel%s.model'%(modelDir,paramExt))
-print 'saved as %s/vhHadModel%s.model'%(modelDir,paramExt)
+#evaluate performance using area under the ROC curve
+vhHadPredYtrain = vhHadModel.predict(trainMatrix)
+vhHadPredYtest  = vhHadModel.predict(testMatrix)
+print 'Training performance:'
+print 'area under roc curve for training set = %1.3f'%( roc_auc_score(vhHadTrainY, vhHadPredYtrain, sample_weight=vhHadTrainFW) )
+print 'area under roc curve for test set     = %1.3f'%( roc_auc_score(vhHadTestY,  vhHadPredYtest,  sample_weight=vhHadTestFW)  )
 
-#build same thing but with equalised weights
-altTrainingDipho = xg.DMatrix(vhHadTrainX, label=vhHadTrainY, weight=vhHadTrainAW, feature_names=vhHadVars)
-print 'about to train alternative vhHad BDT'
-altDiphoModel = xg.train(trainParams, altTrainingDipho)
-print 'done'
-
-#save it
-altDiphoModel.save_model('%s/altDiphoModel%s.model'%(modelDir,paramExt))
-print 'saved as %s/altDiphoModel%s.model'%(modelDir,paramExt)
-
-#check performance of each training
-vhHadPredYxcheck = vhHadModel.predict(trainingDipho)
-vhHadPredY = vhHadModel.predict(testingDipho)
-print 'Default training performance:'
-print 'area under roc curve for training set = %1.3f'%( roc_auc_score(vhHadTrainY, vhHadPredYxcheck, sample_weight=vhHadTrainFW) )
-print 'area under roc curve for test set     = %1.3f'%( roc_auc_score(vhHadTestY, vhHadPredY, sample_weight=vhHadTestFW) )
-
-altDiphoPredYxcheck = altDiphoModel.predict(trainingDipho)
-altDiphoPredY = altDiphoModel.predict(testingDipho)
-print 'Alternative training performance:'
-print 'area under roc curve for training set = %1.3f'%( roc_auc_score(vhHadTrainY, altDiphoPredYxcheck, sample_weight=vhHadTrainFW) )
-print 'area under roc curve for test set     = %1.3f'%( roc_auc_score(vhHadTestY, altDiphoPredY, sample_weight=vhHadTestFW) )
+#check yields for various working points
+testScale = 1./(1.-trainFrac)
+for cutVal in np.arange(0.1,0.91,0.05):
+  selectedSig = opts.intLumi * testScale * np.sum( vhHadTestFW * (vhHadTestY==1) * (vhHadPredYtest>cutVal) )
+  selectedBkg = opts.intLumi * testScale * np.sum( vhHadTestFW * (vhHadTestY==0) * (vhHadPredYtest>cutVal) )
+  print 'Selected events for a cut value of %.2f: S %.3f, B %.3f'%(cutVal, selectedSig, selectedBkg)
